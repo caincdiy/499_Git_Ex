c50bbb1117688329866851689d5fc9d32448a349
caincdiy <caoyueccc@gmail.com>
2020-09-13 08:27:08 -0700

    ascending code

--- 499_Git_Ex/src/sort_array.java
+++ 499_Git_Ex/src/sort_array.java
@@ -5,11 +5,26 @@ public class sort_array {
 		int [] arr1= {2,1,6,3,10,7,8,5,4,9};
 		int [] arr2= {2,1,6,3,10,7,8,5,4,9};
 	
-		
+		ascending(arr1);
+		for(int i=0;i<arr1.length;i++) {
+		System.out.println(arr1[i]);
+	}
 		
 	}//main
 	
 	
+	public static void ascending(int[]arr) {
+		for(int i=0;i<arr.length;i++) {
+			int Min=i;
+			for(int j=i+1;j<arr.length;j++) {
+				if(arr[Min]>arr[j])
+					Min=j;
+			}
+			int temp=arr[i];
+			arr[i]=arr[Min];
+			arr[Min]=temp;
+		}
 	
+}//ascending
 	
 }
