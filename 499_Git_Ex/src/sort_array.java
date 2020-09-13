4069050d0a757e3f78c31cd736ad3d2ee0c699d1
caincdiy <caoyueccc@gmail.com>
2020-09-13 08:25:24 -0700

    start code

---
+++ 499_Git_Ex/src/sort_array.java
@@ -0,0 +1,15 @@
+
+public class sort_array {
+
+	public static void main(String[] args) {
+		int [] arr1= {2,1,6,3,10,7,8,5,4,9};
+		int [] arr2= {2,1,6,3,10,7,8,5,4,9};
+	
+		
+		
+	}//main
+	
+	
+	
+	
+}
